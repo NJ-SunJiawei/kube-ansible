49d33a9a7076f015c261717ab23ac6e091a55ef4
sunjiawei <15996374457@163.com>
2024-06-11 18:25:08 +0800

    update uninstall_k8s.sh

--- tools/uninstall_k8s.sh
+++ tools/uninstall_k8s.sh
@@ -23,8 +23,8 @@ ansible -i ../hosts k8s    -m shell   -a 'rm -rf /usr/lib/systemd/system/{kubele
 echo "remove kubernetes worker OK <<<"
 
 #remove containerd
-ansible -i ../hosts k8s    -m shell   -a 'ctr -n=k8s.io c rm $(ctr -n=k8s.io c ls q)' || exit 1
-ansible -i ../hosts k8s    -m shell   -a 'ctr -n=k8s.io i rm $(ctr -n=k8s.io i ls q)' || exit 1
+ansible -i ../hosts k8s    -m shell   -a 'ctr -n=k8s.io c rm $(ctr -n=k8s.io c ls q) || echo true'
+ansible -i ../hosts k8s    -m shell   -a 'ctr -n=k8s.io i rm $(ctr -n=k8s.io i ls q) || echo true'
 ansible -i ../hosts k8s    -m systemd -a 'name=containerd state=stopped enabled=no' || exit 1
 #ansible -i ../hosts k8s    -m shell   -a 'rm -rf  /etc/containerd' || exit 1
 echo "remove kubernetes containerd OK <<<"
