a3cb49e7869c94d1041b59849a45c1622d7bb9c5
sunjiawei <15996374457@163.com>
2024-06-25 11:09:33 +0800

    keepalived conf update

--- roles/ha/files/check_nginx.sh
+++ roles/ha/files/check_nginx.sh
@@ -2,6 +2,11 @@
 count=$(ps -ef |grep nginx |egrep -cv "grep|$$")
 
 if [ "$count" -eq 0 ];then
+#    systemctl restart nginx
+#    sleep 3
+#    if [ `ps -C nginx --no-header |wc -l` -eq 0 ] ; then
+#        systemctl stop keepalived
+#    fi
     exit 1
 else
     exit 0
