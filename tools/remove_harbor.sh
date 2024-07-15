ef045359100cd11944b2e942b90cdd6c0cffb2a7
sunjiawei <15996374457@163.com>
2024-07-15 11:42:11 +0800

    bug fix

--- tools/remove_harbor.sh
+++ tools/remove_harbor.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 echo "remove harbor start >>>"
-ansible -i ../hosts harbor   -m shell -a 'cd {{ HARBOR_PATH }}/barbor && docker-compose down -v' || exit 1
-#ansible -i ../hosts harbor   -m systemd -a 'name=docker state=stopped enabled=no'
+ansible -i ../hosts harbor   -m shell -a 'docker-compose -f {{ HARBOR_PATH }}/harbor/docker-compose.yml down -v' -uroot || exit 1
+#ansible -i ../hosts harbor   -m systemd -a 'name=docker state=stopped enabled=no' -uroot
 
-ansible -i ../hosts harbor   -m shell -a 'rm -rf /var/log/harbor' || exit 1
-ansible -i ../hosts harbor   -m shell -a 'rm -rf {{ HARBOR_PATH }}/barbor' || exit 1
+ansible -i ../hosts harbor   -m shell -a 'rm -rf /var/log/harbor' -uroot || exit 1
+ansible -i ../hosts harbor   -m shell -a 'rm -rf {{ HARBOR_PATH }}/harbor' -uroot || exit 1
+ansible -i ../hosts harbor   -m shell -a 'rm -rf {{ HARBOR_PATH }}/database' -uroot || exit 1
+ansible -i ../hosts harbor   -m shell -a 'rm -rf {{ HARBOR_PATH }}/registry' -uroot || exit 1
 echo "remove harbor all OK <<<"
