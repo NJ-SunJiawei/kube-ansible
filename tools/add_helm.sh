154efe4503c0a53a78cb337df9a4d9d500771a24
sunjiawei <15996374457@163.com>
2024-07-19 15:30:51 +0800

    prometheus bug fix

--- tools/add_helm.sh
+++ tools/add_helm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "add helm start >>>"
-ansible-playbook -i ../hosts ../04_add-helm.yml -uroot -t helm || exit 1
+ansible-playbook -i ../hosts ../01_install.yml -uroot -t helm || exit 1
 echo "add helm OK <<<"
