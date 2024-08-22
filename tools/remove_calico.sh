a08079b3f57200e11214b33eadf4ae5678dfb27d
sunjiawei <15996374457@163.com>
2024-08-22 11:47:25 +0800

    calico

---
+++ tools/remove_calico.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "remove calico start >>>"
+ansible-playbook -i ../hosts ../01_install.yml -uroot -t addons_delete_calico || exit 1
+echo "add calico yaml <<<"
+
+ansible -i ../hosts k8s  -m shell   -a 'rm -rf /var/lib/cni/calico' || exit 1
+ansible -i ../hosts k8s  -m shell   -a 'rm -rf /etc/cni/net.d/10-calico.conflist' || exit 1
+echo "add calico OK <<<"
