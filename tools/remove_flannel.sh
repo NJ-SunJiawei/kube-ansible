ad963cb87b69e5e9141739c24531ea502dd749ae
sunjiawei <15996374457@163.com>
2024-08-28 19:12:15 +0800

    calico bug fix

--- tools/remove_flannel.sh
+++ tools/remove_flannel.sh
@@ -4,6 +4,5 @@ echo "remove flannel start >>>"
 ansible-playbook -i ../hosts ../01_install.yml -uroot -t addons_delete_flannel || exit 1
 echo "add flannel yaml <<<"
 
-ansible -i ../hosts k8s  -m shell   -a 'rm -rf /var/lib/cni/flannel' || exit 1
 ansible -i ../hosts k8s  -m shell   -a 'rm -rf /etc/cni/net.d' || exit 1
 echo "add flannel OK <<<"
